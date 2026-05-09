0dc88653ccfe87dd0d1f741c07fc8e0cef7b153e
cybro-tech <faizsyukriarta0605@gmail.com>
2026-05-09 23:49:13 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 DB_CONFIG = {
-    'host': os.environ['MYSQLHOST'],
-    'user': os.environ['MYSQLUSER'],
-    'password': os.environ['MYSQLPASSWORD'],
-    'database': os.environ['MYSQLDATABASE'],
-    'port': int(os.environ['MYSQLPORT']),
+    'host': os.environ['DB_HOST'],
+    'user': os.environ['DB_USER'],
+    'password': os.environ['DB_PASSWORD'],
+    'database': os.environ['DB_NAME'],
+    'port': int(os.environ['DB_PORT']),
 }
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'rutan_secret')
