102aef77f760f6ae196218ec2b27e3733096787a
cybro-tech <faizsyukriarta0605@gmail.com>
2026-05-10 00:34:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -851,5 +851,4 @@ def admin_delete(category, id):
     return redirect(url_for('admin_list', category=category))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
